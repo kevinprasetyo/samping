972b3dd4e3ce0d7e92ea45851c3e16b425670c36
Kevin Prasetyo <kevinprasetyokevin@gmail.com>
2023-10-18 09:56:45 +0700

    alhamdulillah2

--- samping.py
+++ samping.py
@@ -43,9 +43,9 @@ fps = int(cap.get(cv2.CAP_PROP_FPS))
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 frame_size = (width, height)
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+# fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
-video_output = cv2.VideoWriter('output.mp4', fourcc, fps, frame_size)
+# video_output = cv2.VideoWriter('output.mp4', fourcc, fps, frame_size)
 
 while cap.isOpened():
     success, img = cap.read()
